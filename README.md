eaf66cc042969acac496c5b6d00b8d782d4311b3
GermanG <german.gutierrez@gmail.com>
2023-05-14 19:21:24 +0200

    first commit with real code

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dnsreverseproxy
-DNS reverse proxy 
+## A very basic DNS reverse proxy 
+
+I started this project because I had a particular itch. 
+I wanted to create a middle DNS that could rewrite my home domain into Consul DNS queries while also routing the 
+remaining queries to a regular DNS. 
+Can consul manage a different domain other than .consul? Of course, but I wanted to get rid of the .service part so
+I have something like gitlab.example.com that goes to the same IP as gitlab.service.consul. 
+I also played with prepared queries, but the documentation I've found online was not enough for me and I ended up
+doing this directly so I don't have a very complicated setup.
+dnsdist is another candidate, but I preferred to do it in golang to learn the language.
+
+## Usage
+```
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --listen value, -l value                                                       Address and port to listen on (default: ":1053")
+   --masqued-domain value, -m value                                               Domain to be masqued (default: ".example.com.")
+   --upstream-domain value, -u value                                              Upstream domain to be masqued (default: ".service.consul.")
+   --upstream-special value, --uc value [ --upstream-special value, --uc value ]  Special upstream host:port (default: "localhost:8600")
+   --upstream-normal value, --un value [ --upstream-normal value, --un value ]    Normal resolution upstream host:port (default: "1.1.1.1:53")
+   --help, -h                                                                     show help
+```
---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/urfave/cli/v2"
+)
+
+/*
+func main() {
+	server := &dns.Server{Addr: ":53", Net: "udp"}
+	dns.HandleFunc(".", handleDNSRequest)
+	log.Fatal(server.ListenAndServe())
+}
+*/
+
+type options struct {
+	upstreamNormal  []string
+	upstreamSpecial []string
+	masquedDomain   string
+	upstreamDomain  string
+}
+
+func getRandomUpstream(upstreams []string) (string, string) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	index := r.Intn(len(upstreams))
+	upstream := upstreams[index]
+	parts := strings.Split(upstream, ":")
+	if len(parts) != 2 {
+		log.Fatalf("Invalid upstream: %s", upstream)
+	}
+	return parts[0], parts[1]
+}
+
+func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg, opts options) {
+	var upstreamHost, upstreamPort string
+	var special bool
+	defer func() {
+		if err := w.Close(); err != nil {
+			log.Println("Error closing connection:", err)
+		}
+	}()
+	c := new(dns.Client)
+	m := new(dns.Msg)
+	log.Println(r.Question[0].Name)
+	if strings.HasSuffix(r.Question[0].Name, opts.masquedDomain) {
+		m.SetQuestion(strings.Replace(r.Question[0].Name, opts.masquedDomain, opts.upstreamDomain, 1), r.Question[0].Qtype)
+		upstreamHost, upstreamPort = getRandomUpstream(opts.upstreamSpecial)
+		special = true
+	} else {
+		m.SetQuestion(r.Question[0].Name, r.Question[0].Qtype)
+		upstreamHost, upstreamPort = getRandomUpstream(opts.upstreamNormal)
+		special = false
+
+	}
+	in, _, err := c.Exchange(m, net.JoinHostPort(upstreamHost, upstreamPort))
+	log.Println(in)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	res := new(dns.Msg)
+	res.SetReply(r)
+	if special {
+		for _, ans := range in.Answer {
+			if ans.Header().Rrtype == dns.TypeA {
+				if a, ok := ans.(*dns.A); ok {
+					a.Hdr.Name = strings.Replace(a.Hdr.Name, opts.upstreamDomain, opts.masquedDomain, 1)
+				}
+			}
+		}
+	}
+	res.Answer = in.Answer
+	log.Println(in.Answer)
+	res.RecursionAvailable = true
+	w.WriteMsg(res)
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "DNS Proxy",
+		Usage: "A DNS proxy server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "listen",
+				Aliases: []string{"l"},
+				Value:   ":1053",
+				Usage:   "Address and port to listen on",
+			},
+			&cli.StringFlag{
+				Name:    "masqued-domain",
+				Aliases: []string{"m"},
+				Value:   ".example.com.",
+				Usage:   "Domain to be masqued",
+			},
+			&cli.StringFlag{
+				Name:    "upstream-domain",
+				Aliases: []string{"u"},
+				Value:   ".service.consul.",
+				Usage:   "Upstream domain to be masqued",
+			},
+			&cli.StringSliceFlag{
+				Name:    "upstream-special",
+				Aliases: []string{"uc"},
+				Value:   cli.NewStringSlice("localhost:8600"),
+				Usage:   "Special upstream host:port",
+			},
+			&cli.StringSliceFlag{
+				Name:    "upstream-normal",
+				Aliases: []string{"un"},
+				Value:   cli.NewStringSlice("1.1.1.1:53"),
+				Usage:   "Normal resolution upstream host:port",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			var opts options
+			listenAddr := c.String("listen")
+			opts.upstreamSpecial = c.StringSlice("upstream-special")
+			opts.upstreamNormal = c.StringSlice("upstream-normal")
+			opts.masquedDomain = c.String("masqued-domain")
+			opts.upstreamDomain = c.String("upstream-domain")
+
+			server := &dns.Server{Addr: listenAddr, Net: "udp"}
+			dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+				handleDNSRequest(w, r, opts)
+			})
+
+			log.Printf("DNS proxy listening on %s\n", listenAddr)
+			return server.ListenAndServe()
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
